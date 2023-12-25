b7abc9c4693f1953f662a7c3245a64e12d47c0b0
Enginnervenkat <32322499+Enginnervenkat@users.noreply.github.com>
2023-12-25 12:34:12 +0530

    added new file

---
+++ DesignPattern/FactoryProgramclass.cs
@@ -0,0 +1,10 @@
+using System;
+
+class FactoryProgramclass
+    {
+        static void Main(string[] args)
+        {
+            // new Client().Main();
+            Console.writeLine("test");
+        }
+    }
